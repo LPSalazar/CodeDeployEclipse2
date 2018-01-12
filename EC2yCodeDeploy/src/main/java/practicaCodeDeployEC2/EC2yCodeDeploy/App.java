0aae39b91270014ac5e9185eca4617e638bf66b6
LPSalazar <Desarrolllo2@192.168.1.102>
2018-01-12 14:38:08 -0300

    wewewewewe

---
+++ EC2yCodeDeploy/src/main/java/practicaCodeDeployEC2/EC2yCodeDeploy/App.java
@@ -0,0 +1,28 @@
+package practicaCodeDeployEC2.EC2yCodeDeploy;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException
+    {
+    	String ruta = "/home/mario/archivo.txt";
+        File archivo = new File(ruta);
+        BufferedWriter bw;
+        if(archivo.exists()) {
+            bw = new BufferedWriter(new FileWriter(archivo));
+            bw.write("El fichero de texto ya estaba creados.");
+        } else {
+            bw = new BufferedWriter(new FileWriter(archivo));
+            bw.write("Acabo de crear el fichero de texto.");
+        }
+        bw.close();
+    }
+}
